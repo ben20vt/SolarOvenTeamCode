2e2d90054dc883f97e85cd94e8d98321cbab4c4a
ben20vt <73235698+ben20vt@users.noreply.github.com>
2021-04-27 13:41:02 -0400

    Add files via upload
    Changed units to Celsius

--- TeamCode.m
+++ TeamCode.m
@@ -7,15 +7,14 @@ clc
 clear
 %% Importing Data
 templogname = input('What is the name of the file containing sensor data?: ', 's');
-
 templog = readtable(templogname);    %Imports the full log of temperature data as a table
 %% Manipulating Imported Data Into a Workable Format for Matlab
 DatetimeStrings = (templog(:,1)); %Pull first column as datetimes
 Dates = table2array(DatetimeStrings); %Convert this table to an array for posixtime
 PosixTime_Calculated = posixtime(Dates); %Converts the datetime in UTC to a Unix timestamp
 %% 
-TimeEnd = max(PosixTime_Calculated);  %Calculates the Unix timestamp of the end date / time by adding the duration in seconds to the starting Unix timestamp
-TimeStart = min(PosixTime_Calculated);    %specifies if the user input start time/date occurs outside the supplied data, either before or below
+TimeEnd = max(PosixTime_Calculated);  %Calculates the Unix timestamp of the end date 
+TimeStart = min(PosixTime_Calculated);    %Calculates the Unix timestamp of the start date 
 %!!!! Notice that both temps are converted to array here so that all data
 %remains the same after this instruction
 templogappended = ([PosixTime_Calculated,table2array(templog(:,2)),table2array(templog(:,3))]);     %Creates a new matrix containing a a column of the datetime timestamps replaced with Unix timestamps for MATLAB to interpret, as well as oven and ambient temperature data
@@ -41,14 +40,14 @@ timeatmax = TimeData(Index);    %Finds the time at the position of the max delta
 timetomax = (timeatmax - TimeStart);
 dateatmax = datetime(timeatmax, 'Format','MMMM dd, yyyy HH:mm:ss', 'TimeZone', 'America/New_York', 'ConvertFrom', 'posixtime');     %Converts the timestamp of the time of occurance of max delta T into a datetime string. This makes it more readable in the code output
 disp(' ');   %Adds a line break in the output window to improve readability
-fprintf('The maximum temperature difference during testing was: %d °F and occured on %s.  \n',Max,dateatmax);    %Displays the max delta T and time of occurance
-fprintf("This means on %s the oven reached it's greatest temperature above the ambient temperature during testing, and took %d seconds to reach this temperature. \n",dateatmax,timetomax);
+fprintf('The maximum temperature difference during testing was: %d °C and occured on %s.  \n',Max,dateatmax);    %Displays the max delta T and time of occurance
+fprintf("This means on %s the oven reached its greatest temperature above the ambient temperature during testing, and took %d seconds to reach this temperature. \n",dateatmax,timetomax);
 %% Calculating Efficiency
     %% Qab
     Tf = Max;    %Calculates the average temp difference from the data
-    DeltaT = (((Tf-32)*(5/9))+273.15);  %Converts the temperature difference to kelvin
+    DeltaT = (Tf + 273);  %Converts the temperature difference to kelvin
     heatCap = 1000;  %Specific heat of air
-    VolM = 0.020816; %Volume of air
+    VolM = 0.0216411; %Volume of air
     Rho = 1.225;  %Density of air
     Qab = VolM*Rho*heatCap*DeltaT;    %Calculates Qab
     %% Qin  
@@ -56,16 +55,17 @@ fprintf("This means on %s the oven reached it's greatest temperature above the a
     irradiance = 3690;   %Average incidence in Blacksburg in March
     t = (timeatmax - TimeStart)/3600;  %duration to max diff in temp in hours
     Qin = (((irradiance*AreaM)/12)*t);   %Calculates Qin
-n = Qab/Qin;    %Calculates efficiency
+n = (Qab/Qin);    %Calculates efficiency
 disp(' ');   %Adds a line break in the output window to improve readability
 disp("The estimated efficiency of the solar oven is: " + n*100 + "%");  %displays the calculated efficiency
 %% Calculate and display T max for given time of year or location
 ijuly = 5500;    %average incidence in July in Blacksburg
-tdiffjuly = ((Rho*VolM*heatCap*DeltaT)/((ijuly)*AreaM*n));    %Theoretical max temp difference in July in Blacksburg
-tjulyinternal = tdiffjuly + 90;     %Calculates the theoretical max oven temp in July with an ambient temp of 90°F 
+tdiffjuly = ((n*AreaM*((ijuly/12)*t))/(Rho*VolM*heatCap));
+%tdiffjuly = ((Rho*VolM*heatCap*DeltaT*n)/((ijuly)*AreaM));    %Theoretical max temp difference in July in Blacksburg
+tjulyinternal = tdiffjuly + 32;     %Calculates the theoretical max oven temp in July with an ambient temp of 90°F 
 disp(' ');   %Adds a line break in the output window to improve readability
-disp("The theoretical maximum achievable temperature gradient for July in Blacksburg is: " + tdiffjuly + "°F. Assuming an ambient temperature of 90°F, this means the oven is capable of reaching internal temperatures of " + tjulyinternal + "°F");  %Displays the theoretical max temp gradient in July in Blacksburg w/ an assumed ambient temp of 90°F
+disp("The theoretical maximum achievable temperature gradient for July in Blacksburg is: " + tdiffjuly + "°C. Assuming an ambient temperature of 32°C, this means the oven is capable of reaching internal temperatures of " + tjulyinternal + "°C");  %Displays the theoretical max temp gradient in July in Blacksburg w/ an assumed ambient temp of 90°F
 %% Exports dates, time, temperatures, and temp difference to excel file
-vars={'Dates','Unix Timestamp (s)','Oven Temperature (°F)','Ambient Temperature (°F)','Temperature Difference (°F)'};
+vars={'Dates','Unix Timestamp (s)','Oven Temperature (°C)','Ambient Temperature (°C)','Temperature Difference (°C)'};
 excel_export_data = table(DateTimesConverted,TimeData,OvenTempData,AmbientTempData,diff,'VariableNames',vars);   %Concats the specific vectors of values into a single matrix
 writetable(excel_export_data,'ExportData.xlsx');
